67affba378acc85235ebeda523c59bc8983ce5b0
Mav-lit <ngonie3mav@gmail.com>
2022-10-07 13:54:17 +0200

    Main Class

--- DairyFarm.java
+++ DairyFarm.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
